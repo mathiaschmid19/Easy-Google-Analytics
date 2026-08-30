d286aae70b8d6a0a6a52d83272960163ce26ea79
mathiaschmid19 <mathias.schmid19@hotmail.com>
2026-08-30 22:32:28 +0300

    Restore GA4 wp_head output as temporary EGA_Settings::output()

--- includes/class-settings.php
+++ includes/class-settings.php
@@ -9,6 +9,7 @@ class EGA_Settings {
         add_action('admin_menu', array(__CLASS__, 'menu'));
         add_action('admin_init', array(__CLASS__, 'register_settings'));
         add_action('admin_init', array(__CLASS__, 'register_fields'));
+        add_action('wp_head', array(__CLASS__, 'output'), 10);
     }
 
     public static function menu() {
@@ -93,5 +94,24 @@ class EGA_Settings {
         $ga4_code = get_option('for_you_google_analytics_ga4_code');
         echo '<input type="text" name="for_you_google_analytics_ga4_code" value="' . esc_attr($ga4_code) . '" placeholder="G-XXXXXXXXXX" class="regular-text" />';
         echo '<p class="description">' . esc_html__('Format: G-XXXXXXXXXX', 'for-you-google-analytics') . '</p>';
+    }
+
+    // TEMPORARY: relocated here from the pre-refactor plugin file to avoid breaking wp_head output. Removed when Task 4 introduces EGA_Tracking_Output.
+    public static function output() {
+        $ga4_code = get_option('for_you_google_analytics_ga4_code');
+
+        if (!empty($ga4_code)) {
+            ?>
+            <!-- Global site tag (gtag.js) - Google Analytics (GA4) -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo esc_attr($ga4_code); ?>"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag() { dataLayer.push(arguments); }
+                gtag('js', new Date());
+
+                gtag('config', '<?php echo esc_js($ga4_code); ?>');
+            </script>
+            <?php
+        }
     }
 }
